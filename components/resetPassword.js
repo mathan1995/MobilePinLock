4bb91c9c8a90f432f358b597dc696d67d6491c46
mathan1995 <mathanganjeya@gmail.com>
2019-07-03 00:17:38 +0530

    Component Division

--- components/resetPassword.js
+++ components/resetPassword.js
@@ -1,46 +1,60 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 const Reset = props => (
-  <View style={styles.resetContainer}>
-    <View
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%"
-      }}
-    >
-      <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-        Reset your Password
-      </Text>
-    </View>
-    <View
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%"
-      }}
-    >
-      <TouchableOpacity onPress={() => props.toggleViewFunc()}>
-        <Text>Enter Your Password again</Text>
-      </TouchableOpacity>
-    </View>
+  <View style={styles.layout}>
+    <View style={styles.resetContainer}>
+      <View>
+        <Image
+          style={{ width: 120, height: 120 }}
+          source={require("./assets/key.png")}
+        />
+      </View>
+      <View
+        style={{
+          marginTop: 50,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%"
+        }}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 20, color: "#00416A" }}>
+          Reset your PIN
+        </Text>
+      </View>
+      <View
+        style={{
+          marginTop: 50,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%"
+        }}
+      >
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/back.png")}
+        />
+        <TouchableOpacity onPress={() => props.toggleViewFunc()}>
+          <Text>Enter PIN again</Text>
+        </TouchableOpacity>
+      </View>
 
-    <View
-      style={{
-        marginTop: 200,
-        padding: 10,
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FFFF"
-      }}
-    >
-      <TouchableOpacity onPress={() => props.resetPin()}>
-        <View style={{ width: "100%", backgroundColor: "#FFF" }}>
-          <Text style={{ color: "#fff" }}>Reset PIN</Text>
-        </View>
-      </TouchableOpacity>
+      <View
+        style={{
+          marginTop: 30,
+          padding: 10,
+          width: "80%",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#00416A"
+        }}
+      >
+        <TouchableOpacity onPress={() => props.resetPin()}>
+          <View style={{ width: "100%", backgroundColor: "#00416A" }}>
+            <Text style={{ color: "#fff" }}>Reset PIN</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   </View>
 );
@@ -48,17 +62,17 @@ const Reset = props => (
 const styles = StyleSheet.create({
   resetContainer: {
     flex: 1,
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    marginLeft: 20,
-    marginBottom: 20,
-
-    bottom: 0,
-    margin: 10,
-
+    justifyContent: "flex-start",
+    paddingTop: 100,
+    margin: 15,
+    borderRadius: 5,
     alignItems: "center",
-    backgroundColor: "#fafafa"
+    backgroundColor: "#FFF",
+    elevation: 2
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: "#000"
   }
 });
 
